052af00877f64b45abe5a8a4ff6df3fb9abeed0b
Chris von Csefalvay <chris@chrisvoncsefalvay.com>
2016-07-03 19:56:59 +0200

    setup.py added and prepped for pypi upload.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='processpathway',
+    version='0.3.19',
+    packages=['processpathway', 'processpathway.examples', 'processpathway.processpathway'],
+    url='https://www.github.com/chrisvoncsefalvay/processpathway',
+    license='MIT',
+    author='Chris von Csefalvay',
+    author_email='chris@chrisvoncsefalvay.com',
+    description='A nifty little toolkit to create stress-free, frustrationless image processing pathways from your webcam for computer vision experiments. Or observing your cat.'
+)
